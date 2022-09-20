70a61ade632a4a9a5526bf557898a9a0a5fc7217
yoon <73841260+bgyoons@users.noreply.github.com>
2022-09-20 21:44:43 +0900

    Lv2. 68% 완전 탐색

---
+++ 카펫.js
@@ -0,0 +1,15 @@
+function solution(brown, yellow) {
+    var answer = [];
+    let middle = []
+    
+    for (let i = yellow; i >= Math.sqrt(yellow); i--) {
+        if (yellow % i === 0 && i >= yellow / i) middle = [i, yellow / i]
+        
+        let [first, second] = middle
+        let output = (first + 2) * 2 + second * 2
+        
+        if (output === brown) answer = [first + 2, second + 2]
+    }
+    
+    return answer;
+}
