9e44de41193e682c907f47bf4c36534ad3475ddb
yoon <73841260+bgyoons@users.noreply.github.com>
2022-09-21 00:16:43 +0900

    Lv2. 67%

---
+++ 영어 끝말잇기.js
@@ -0,0 +1,20 @@
+function solution(n, words) {
+    var answer = [0, 0]
+
+    for (let i = 0; i < words.length; i++) {
+        let index = 0
+    
+        if (i !== 0 && words[i - 1][words[i - 1].length - 1] !== words[i][0]) index = i + 1
+        else {
+            for (let j = i - 1; j >= 0; j--) {
+              if (words[i] === words[j]) index = i + 1
+            }
+        }
+        
+        if (index !== 0) {
+            return [index % n === 0 ? n : index % n, Math.ceil(index / n)]
+        }
+    }
+
+    return answer;
+}
