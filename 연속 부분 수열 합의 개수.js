7f454c392521d85d0763e838169eec06116151d8
yoon <73841260+bgyoons@users.noreply.github.com>
2022-10-18 02:22:24 +0900

    Lv2

--- 연속 부분 수열 합의 개수.js
+++ 연속 부분 수열 합의 개수.js
@@ -3,7 +3,7 @@ function solution(elements) {
     let result = elements.slice()
     
     for (let i = 1; i < elements.length; i++) {
-        result = result.map((item, index) =>i + index >= elements.length ? item + elements[i + index - elements.length] : item + elements[index + i])
+        result = result.map((item, index) => i + index >= elements.length ? item + elements[i + index - elements.length] : item + elements[index + i])
         answer.push(...result)
     }
     return [...new Set(answer)].length;
