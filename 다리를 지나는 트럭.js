cade2949c85df45e4f7ec2ef466b3103767f1114
yoon <73841260+bgyoons@users.noreply.github.com>
2022-11-22 10:30:19 +0900

    Lv2. 큐/스택

---
+++ 다리를 지나는 트럭.js
@@ -0,0 +1,23 @@
+function solution(bridge_length, weight, truck_weights) {
+    let bridge = new Array(bridge_length).fill(0)
+    let sum = truck_weights.shift()
+    bridge.shift()
+    bridge.push(sum)
+    var answer = 1;
+    
+    while (sum > 0) {
+        let truck = truck_weights[0]
+        sum -= bridge.shift()
+        
+        if (sum + truck <= weight) {
+            bridge.push(truck)
+            sum += truck
+            truck_weights.shift()
+        } else {
+            bridge.push(0)            
+        }
+        answer++
+    }
+    
+    return answer;
+}
