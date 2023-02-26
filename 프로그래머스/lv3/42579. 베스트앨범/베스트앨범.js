5bdd6278f58254e07f8f2940b37f047ed4e49ecd
yoon <73841260+bgyoons@users.noreply.github.com>
2023-02-26 16:39:54 +0900

    [level 3] Title: 베스트앨범, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.js
@@ -0,0 +1,46 @@
+function solution(genres, plays) {
+    var answer = [];
+    let musicInfo = []; // 음악 정보
+    let generesCount = {} // 장르별 재생수
+    let genresPlayRank = [...new Set(genres)]; // 장르 재생수 순위
+    
+    // 장르 재생 랭킹
+    genres.forEach((ele, idx) => {
+        if (generesCount[ele]) {
+            generesCount[ele] += plays[idx]
+        } else generesCount[ele] = plays[idx]
+    })
+    
+    genresPlayRank = genresPlayRank.map((ele, idx) => [ele, generesCount[ele]])
+                        .sort((a, b) => b[1] - a[1])
+                        .map(ele => ele[0])
+    
+    // 음악 정보
+    genres.forEach((ele, idx) => {
+        let obj = {}
+        obj.genre = ele;
+        obj.count = plays[idx]
+        obj.index = idx
+        musicInfo.push(obj)
+    })
+    
+    musicInfo.sort((a, b) => {
+        if (b.count === a.count && b.genre === a.genre) {
+            return a.index - b.index
+        } else return b.count - a.count
+    })
+    
+    genresPlayRank.forEach(ele => {
+        let count = 0;
+        for (let info of musicInfo) {
+            if (count === 2) {
+              continue;  
+            } else if (info.genre === ele) {
+                answer.push(info.index)
+                count += 1
+            }
+        }
+    })
+    
+    return answer;
+}
