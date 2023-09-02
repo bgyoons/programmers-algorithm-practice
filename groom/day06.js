b022129c970e9f12e02cd3a6be362c1185e15718
Baeg <73841260+bgyoons@users.noreply.github.com>
2023-09-03 03:58:02 +0900

    구름챌린지 6일차

--- groom/day06.js
+++ groom/day06.js
@@ -8,28 +8,39 @@ let rl = readline.createInterface({
 let input = [];
 rl.on('line', (line) => {
   input.push(line);
-  if (line.length === input[0]) rl.close();
+  if (input.length === 2) rl.close();
 });
 
 rl.on('close', () => {
-  const [count, str] = input;
-  const maxLength = +count - 2;
-  const dic = str.split('');
-  let P = dic.slice();
-  let strArr = [];
+  const [N, S] = input;
+  let P = new Set();
+  let wordList = [];
 
-  for (let i = 0; i <= maxLength; i++) {
-    const first = str.slice(0, i + 1);
-    for (let j = i + 1; j <= maxLength; j++) {
-      const second = str.slice(i + 1, j + 1);
-      const third = str.slice(j + 1);
-      strArr.push([first, second, third]);
-      if (!P.includes(first)) P.push(first);
-      if (!P.includes(second)) P.push(second);
-      if (!P.includes(third)) P.push(third);
+  for (let i = 1; i < N - 1; i++) {
+    for (let j = i + 1; j < N; j++) {
+      const first = S.slice(0, i);
+      const second = S.slice(i, j);
+      const third = S.slice(j);
+
+      wordList.push([first, second, third]);
+      P.add(first);
+      P.add(second);
+      P.add(third);
     }
-    P.sort();
   }
-  const sum = strArr.map(arr => arr.reduce((acc, cur) => acc += P.indexOf(cur) + 1, 0));
-  console.log(Math.max(...sum));
+
+  const dictionary = [...P].sort();
+  let maxScore = 0;
+
+  for (const list of wordList) {
+    let score = 0;
+    for (const word of list) {
+      score += dictionary.indexOf(word) + 1;
+    }
+    if (score > maxScore) maxScore = score;
+  }
+
+  console.log(maxScore);
+  // const score = cases.map((arr, idx) => arr.reduce((acc, cur) => acc += P.indexOf(cur) + 1, 0));
+  // console.log(Math.max(...score)); 
 })
