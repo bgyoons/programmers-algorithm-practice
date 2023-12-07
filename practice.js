c1f3154fae42e24870896a78500bc221b21fb48d
Baeg <73841260+bgyoons@users.noreply.github.com>
2023-12-07 12:09:42 +0900

    [백준 2606] 바이러스

--- practice.js
+++ practice.js
@@ -1,6 +1,29 @@
 const fs = require("fs");
-let input = fs.readFileSync("./input.txt").toString().trim().split("\n"); // dev/stdin
+// let input = fs.readFileSync("dev/stdin").toString().trim().split("\n");
+let input = fs.readFileSync("./input.txt").toString().trim().split("\n");
 
-const answer = [];
+const [number, , ...pairs] = input;
+const graph = pairs.reduce((acc, cur) => {
+  const [from, to] = cur.split(" ");
+  !acc[from] ? (acc[from] = [to]) : acc[from].push(to);
+  !acc[to] ? (acc[to] = [from]) : acc[to].push(from);
+  return acc;
+}, {});
 
-console.log(answer);
+let answer = 0;
+const visited = new Array(+number + 1).fill(false);
+const queue = [1];
+
+while (queue.length) {
+  const cur = queue.shift();
+
+  if (visited[cur]) continue;
+
+  visited[cur] = true;
+  answer += 1;
+
+  for (const computer of graph[cur]) {
+    if (!visited[computer]) queue.push(computer);
+  }
+}
+console.log(answer - 1);
