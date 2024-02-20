ab3219beb58bf55c491fdd779ab39ce643b740ef
Baeg <73841260+bgyoons@users.noreply.github.com>
2024-02-20 19:05:09 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 828 ms, Memory: 93380 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.js
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.js
@@ -1,23 +1,37 @@
 const fs = require("fs");
 let input = fs.readFileSync("dev/stdin").toString().trim().split("\n");
 
-const [N, ...rest] = input;
-const graph = new Array(+N + 1).fill().map(() => new Array());
-rest.forEach((pair) => {
-  const [from, to] = pair.split(" ").map(Number);
-  graph[from].push(to);
-  graph[to].push(from);
-});
-const answer = new Array(+N + 1).fill();
-const visited = new Array(+N + 1).fill(false);
-const dfs = (lv) => {
-  if (visited[lv]) return;
-  visited[lv] = true;
-  graph[lv].forEach((node) => {
-    if (!visited[node]) answer[node] = lv;
-    dfs(node);
-  });
+const N = +input.shift();
+const tree = new Array(N + 1).fill().map(() => new Array());
+
+for (const str of input) {
+  const [x, y] = str.split(" ").map(Number);
+  tree[x].push(y);
+  tree[y].push(x);
+}
+
+const parent = new Array(N + 1).fill(0);
+const visited = new Array(N + 1).fill(false);
+const stack = [];
+const dfs = () => {
+  const cur = stack.shift();
+
+  if (visited[cur]) return;
+  visited[cur] = true;
+
+  for (const child of tree[cur]) {
+    if (visited[child]) continue;
+    parent[child] = cur;
+    stack.push(child);
+    dfs();
+  }
 };
 
-dfs(1);
-console.log(answer.slice(2).join("\n"));
+for (let i = 1; i <= N; i++) {
+  if (visited[i]) continue;
+  stack.push(i);
+  dfs();
+}
+
+const answer = parent.filter((ele) => ele);
+console.log(answer.join("\n"));
